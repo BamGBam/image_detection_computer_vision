b2b44c93c564612cf62742169aebf2d525aa33fa
BamGBam <bamdadgoshtasbi@berkeley.edu>
2024-06-14 13:21:11 -0700

    Updated CI/CD

--- src/facerecognition_upload.py
+++ src/facerecognition_upload.py
@@ -12,7 +12,6 @@ haar_cascade = cv2.CascadeClassifier('Haarcascade_frontalface_default.xml')
   
 # Applying the face detection method on the grayscale image 
 faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9) 
-  
 # Iterating through rectangles of detected faces 
 for (x, y, w, h) in faces_rect: 
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2) 
