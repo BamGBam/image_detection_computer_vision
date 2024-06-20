5f8f184cfaec54fe48bf49f451ae04017a80ffd5
RameshOswal <rameshoswal@gmail.com>
2024-06-20 15:41:56 -0700

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
 
+st.title('Bam\'s Stop Sign Detector')
+
+uploaded_file = st.file_uploader("Upload a jpg/png file", type=["jpg", "png"])
+if uploaded_file is not None:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.image(bytes_data)
