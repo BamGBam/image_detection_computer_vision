b7e78ccbe3259aa2e5ae755d8c968a9d8203d397
RameshOswal <rameshoswal@gmail.com>
2024-06-21 12:28:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import os
 
 st.title('Bam\'s Stop Sign Detector')
 
@@ -8,3 +9,9 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
     st.image(bytes_data)
+
+    
+    st.write("Output filename:", uploaded_file.name)
+
+    
+    
