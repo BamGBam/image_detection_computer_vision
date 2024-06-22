b171dbc13ff36389e951c7af4505f2e78da46d8e
RameshOswal <rameshoswal@gmail.com>
2024-06-22 01:30:41 +0000

    move function out

---
+++ classification_face_stop_sign_recognition.py
@@ -0,0 +1,46 @@
+import fire
+
+import cv2 
+from matplotlib import pyplot as plt
+
+
+def run_classification(input_image_path: str ='raw-data/image_6.jpg', output_image_path: str ='Detected_Faces_and_Stop_Signs.png'):
+    # Reading the image 
+    # img = cv2.imread("/home/bgoshtasbi/Desktop/ML Project/proj1_classification/raw-data/image_6.jpg")
+    print("Running classification")
+    img = cv2.imread(input_image_path)
+
+    # proj1_classification/src/Haarcascade_frontalface_default.xml
+    # Converting image to grayscale 
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Loading the required xml classifier files 
+    # stop_cascade = cv2.CascadeClassifier('/path_to_your_xml_file/stop_data.xml')
+
+    face_cascade = cv2.CascadeClassifier('src/Haarcascade_frontalface_default.xml') 
+    stop_cascade = cv2.CascadeClassifier('src/stop_data.xml')
+
+    # Applying face detection method on the grayscale image 
+    faces_rect = face_cascade.detectMultiScale(gray_img, 1.1, 9,) 
+
+    # Detecting stop signs
+    stop_signs = stop_cascade.detectMultiScale(gray_img, minSize =(20, 20))
+
+    # Draw rectangles around detected faces and stop signs 
+    for (x, y, w, h) in faces_rect: 
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2) 
+
+    for (x, y, width, height) in stop_signs:
+        cv2.rectangle(img, (x, y), (x + height, y + width), (0, 255, 0), 5)
+        
+    # Display image
+    # Save image instead of displaying it
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.savefig(output_image_path)
+
+    # cv2.imshow('Detected Faces and Stop Signs', img) 
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+if __name__=="__main__":
+    fire.Fire(run_classification)
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import os
 import sys
-sys.path.append('./src')
-from src.classification_face_stop_sign_recognition import run_classification
+
+from classification_face_stop_sign_recognition import run_classification
 
 st.title('Bam\'s Stop Sign Detector')
 
